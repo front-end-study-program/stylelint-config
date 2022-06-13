d58859e7a032eb79bdf2361c9de84114b1b4cdb3
yanquanfahei <1449826851@qq.com>
2022-06-13 22:33:45 +0800

    feat: init basic scss

---
+++ README.md
@@ -0,0 +1,7 @@
+# stylelint rules config
+
+自定义 stylelint 共享配置
+
+## TODO
+
+1. scss rules 不生效排查
--- packages/basic-scss/index.js
+++ packages/basic-scss/index.js
@@ -1,5 +1,10 @@
 module.exports = {
   extends: [
     "stylelint-config-standard-scss"
-  ]
+  ],
+  rules: {
+    'max-empty-lines': 1,
+    'block-opening-brace-space-before': 'always',
+    'declaration-block-single-line-max-declarations': 1,
+  }
 }
